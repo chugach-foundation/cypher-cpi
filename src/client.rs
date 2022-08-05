d7663aa94d5ed29e86109752e85092a4f76a90bf
hoak <murlux@protonmail.com>
2022-08-05 22:03:52 +0100

    adding more client methods

--- src/client.rs
+++ src/client.rs
@@ -14,6 +14,12 @@ use {
     solana_sdk::{instruction::Instruction, sysvar::SysvarId},
 };
 
+// binary strings
+pub const B_CYPHER_GROUP: &[u8] = b"cypher_group";
+pub const B_CYPHER_USER: &[u8] = b"cypher_user";
+pub const B_DEX_MARKET_AUTHORITY: &[u8] = b"dex_market_authority";
+pub const B_OPEN_ORDERS: &[u8] = b"open_orders";
+
 pub fn gen_dex_vault_signer_key(nonce: u64, dex_market_pk: &Pubkey) -> Pubkey {
     let seeds = [dex_market_pk.as_ref(), bytes_of(&nonce)];
     Pubkey::create_program_address(&seeds, &dex::id()).unwrap()
@@ -21,10 +27,34 @@ pub fn gen_dex_vault_signer_key(nonce: u64, dex_market_pk: &Pubkey) -> Pubkey {
 
 pub fn derive_dex_market_authority(dex_market_pk: &Pubkey) -> Pubkey {
     Pubkey::find_program_address(
-        &[b"dex_market_authority", dex_market_pk.as_ref()],
+        &[B_DEX_MARKET_AUTHORITY, dex_market_pk.as_ref()],
         &crate::id(),
     )
     .0
+}
+
+pub fn derive_cypher_user_address(cypher_group_pk: &Pubkey, owner_pk: &Pubkey) -> (Pubkey, u8) {
+    let (address, bump) = Pubkey::find_program_address(
+        &[
+            B_CYPHER_USER,
+            cypher_group_pk.as_ref(),
+            &owner_pk.to_bytes(),
+        ],
+        &crate::id(),
+    );
+
+    (address, bump)
+}
+
+pub fn derive_open_orders_address(dex_market_pk: &Pubkey, cypher_user_pk: &Pubkey) -> (Pubkey, u8) {
+    Pubkey::find_program_address(
+        &[
+            B_OPEN_ORDERS,
+            dex_market_pk.as_ref(),
+            cypher_user_pk.as_ref(),
+        ],
+        &crate::id(),
+    )
 }
 
 pub fn init_cypher_user_ix(
