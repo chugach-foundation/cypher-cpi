96e55098220323bfb27034de8852714c791c2de7
hoak <hoak@offpiste.io>
2022-08-10 15:48:41 -0600

    updating idl and adding delegation methods

--- cypher/src/client.rs
+++ cypher/src/client.rs
@@ -53,6 +53,82 @@ pub fn init_cypher_user_ix(
 }
 
 #[cfg(feature = "client")]
+pub fn create_cypher_user_ix(
+    cypher_group: &Pubkey,
+    cypher_user: &Pubkey,
+    owner: &Pubkey,
+    payer: &Pubkey,
+    bump: u8,
+    account_number: u64,
+) -> Instruction {
+    let accounts = CreateCypherUser {
+        cypher_group: *cypher_group,
+        cypher_user: *cypher_user,
+        owner: *owner,
+        payer: *payer,
+        system_program: system_program::ID,
+    };
+    let ix_data = crate::instruction::CreateCypherUser {
+        _bump: bump,
+        _account_number: account_number
+    };
+    Instruction {
+        accounts: accounts.to_account_metas(Some(false)),
+        data: get_ix_data(
+            "create_cypher_user",
+            AnchorSerialize::try_to_vec(&ix_data).unwrap(),
+        ),
+        program_id: crate::id(),
+    }
+}
+
+#[cfg(feature = "client")]
+pub fn close_cypher_user_ix(
+    cypher_group: &Pubkey,
+    cypher_user: &Pubkey,
+    owner: &Pubkey,
+) -> Instruction {
+    let accounts = CloseCypherUser {
+        cypher_group: *cypher_group,
+        cypher_user: *cypher_user,
+        user_signer: *owner,
+    };
+    let ix_data = crate::instruction::CloseCypherUser {};
+    Instruction {
+        accounts: accounts.to_account_metas(Some(false)),
+        data: get_ix_data(
+            "close_cypher_user",
+            AnchorSerialize::try_to_vec(&ix_data).unwrap(),
+        ),
+        program_id: crate::id(),
+    }
+}
+
+#[cfg(feature = "client")]
+pub fn set_delegate_ix(
+    cypher_group: &Pubkey,
+    cypher_user: &Pubkey,
+    owner: &Pubkey,
+    delegate: &Pubkey,
+) -> Instruction {
+    let accounts = SetDelegate {
+        cypher_group: *cypher_group,
+        cypher_user: *cypher_user,
+        user_signer: *owner,
+        delegate: *delegate,
+    };
+    let ix_data = crate::instruction::SetDelegate {};
+    Instruction {
+        accounts: accounts.to_account_metas(Some(false)),
+        data: get_ix_data(
+            "set_delegate",
+            AnchorSerialize::try_to_vec(&ix_data).unwrap(),
+        ),
+        program_id: crate::id(),
+    }
+}
+
+#[cfg(feature = "client")]
 pub fn deposit_collateral_ix(
     cypher_group: &Pubkey,
     cypher_user: &Pubkey,
